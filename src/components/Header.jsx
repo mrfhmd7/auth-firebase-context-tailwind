0897ad26e86beadd17aaf7f444a7dee2b340d10c
mrfhmd7 <mrf.hmd7@gmail.com>
2024-07-25 03:44:01 +0600

    add navbar in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
      return (
-          <div>
-               <h3>This is Header</h3>
-          </div>
+          <nav>
+               <div className="navbar bg-neutral text-neutral-content">
+                    <button className="btn btn-ghost text-xl">Auth Master</button>
+                    <Link className="btn btn-ghost text-xl" to="/">Home</Link>
+                    <Link className="btn btn-ghost text-xl" to="/login">Login</Link>
+                    <Link className="btn btn-ghost text-xl" to="/register">Register</Link>
+               </div>
+          </nav>
      );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./layout/Main.jsx";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<div></div>
-  }
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

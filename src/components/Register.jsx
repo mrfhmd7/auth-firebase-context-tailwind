8f19e987de81139ada10d48186b7a6ea122c1725
mrfhmd7 <mrf.hmd7@gmail.com>
2024-07-26 02:55:42 +0600

    login and register form added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
      return (
-          <div>
-               <h2>Please Register</h2>
+          <div className="hero bg-base-200 min-h-screen">
+               <div className="hero-content flex-col">
+                    <div className="text-center lg:text-left">
+                         <h1 className="text-5xl font-bold">Please Register</h1>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                         <form className="card-body">
+                              <div className="form-control">
+                                   <label className="label">
+                                        <span className="label-text">Email</span>
+                                   </label>
+                                   <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                              </div>
+                              <div className="form-control">
+                                   <label className="label">
+                                        <span className="label-text">Password</span>
+                                   </label>
+                                   <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                                   <label className="label">
+                                        <Link to="/login" className="label-text-alt link link-hover">Already have an account?</Link>
+                                   </label>
+                              </div>
+                              <div className="form-control mt-6">
+                                   <button className="btn btn-primary">Login</button>
+                              </div>
+                         </form>
+                    </div>
+               </div>
           </div>
      );
 };

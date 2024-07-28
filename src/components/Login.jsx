cf96ccaeeab7369f781b4339b2ee5a2b034c36fa
mrfhmd7 <mrf.hmd7@gmail.com>
2024-07-29 02:10:54 +0600

    implemented login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Login = () => {
+
+     const { signIn } = useContext(AuthContext);
 
      const handleLogin = event => {
           event.preventDefault();
@@ -9,7 +12,16 @@ const Login = () => {
           const email = form.email.value;
           const password = form.password.value;
           console.log(email, password);
-     }
+
+          signIn(email, password)
+               .then(result => {
+                    const loggedInUser = result.user;
+                    console.log(loggedInUser);
+               })
+               .catch(error => {
+                    console.log(error);
+               })
+     };
 
      return (
           <div className="hero bg-base-200 min-h-screen">

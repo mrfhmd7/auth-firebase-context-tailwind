37ac3220e3c29f51f7917475f56effa73d52a238
mrfhmd7 <mrf.hmd7@gmail.com>
2024-07-30 23:55:15 +0600

    show user and sign out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Header = () => {
+
+     const { user, logOut } = useContext(AuthContext);
+
+     const handleLogOut = () => {
+          logOut()
+               .then(() => { })
+               .catch(error => console.error(error))
+     };
+
      return (
           <nav>
                <div className="navbar bg-neutral text-neutral-content">
@@ -9,6 +19,15 @@ const Header = () => {
                     <Link className="btn btn-ghost text-xl" to="/">Home</Link>
                     <Link className="btn btn-ghost text-xl" to="/login">Login</Link>
                     <Link className="btn btn-ghost text-xl" to="/register">Register</Link>
+                    {
+                         user ?
+                              <>
+                                   <span>{user.email}</span>
+                                   <button onClick={handleLogOut} className="btn btn-sm">Sign out</button>
+                              </>
+                              :
+                              <Link to="/login"></Link>
+                    }
                </div>
           </nav>
      );

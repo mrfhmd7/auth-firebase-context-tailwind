df8e4d3a3f4dc993defcaf264b680ce422daf7fb
mrfhmd7 <mrf.hmd7@gmail.com>
2024-08-24 22:57:30 +0600

    private router created

---
+++ src/components/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+     return (
+          <div>
+               <h2>Place your orders here!!</h2>
+          </div>
+     );
+};
+
+export default Orders;
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import Home from "./components/Home.jsx";
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import AuthProviders from "./providers/AuthProviders.jsx";
+import Orders from "./components/Orders.jsx";
+import PrivateRoute from "./routes/PrivateRoute.jsx";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +28,10 @@ const router = createBrowserRouter([
         path: "/register",
         element: <Register />,
       },
+      {
+        path: "/orders",
+        element: <PrivateRoute> <Orders></Orders> </PrivateRoute>,
+      }
     ],
   },
 ]);

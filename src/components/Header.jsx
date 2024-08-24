816eae1ad3512bc2cdd825889f281b24d7ad22a2
mrfhmd7 <mrf.hmd7@gmail.com>
2024-08-24 23:20:40 +0600

    hide private route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,10 @@ const Header = () => {
                <div className="navbar bg-neutral text-neutral-content">
                     <button className="btn btn-ghost text-xl">Auth Master</button>
                     <Link className="btn btn-ghost text-xl" to="/">Home</Link>
-                    <Link className="btn btn-ghost text-xl" to="/profile">Profile</Link>
+                    {
+                         user &&
+                         <Link className="btn btn-ghost text-xl" to="/profile">Profile</Link>
+                    }
                     <Link className="btn btn-ghost text-xl" to="/orders">Orders</Link>
                     <Link className="btn btn-ghost text-xl" to="/login">Login</Link>
                     <Link className="btn btn-ghost text-xl" to="/register">Register</Link>
